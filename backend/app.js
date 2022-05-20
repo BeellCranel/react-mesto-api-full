e3ce6184170b9df86f6d9c2adcb9306af52849cf
Dergach Roman <dergachroma@gmail.com>
2022-05-20 15:58:22 +0300

    feat: реализовал корс

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const cors = require('./middlewares/cors');
 const routes = require('./routes');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+app.use(cors);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -15,17 +15,21 @@ router.post('/cards', celebrate({
     link: Joi.string().required().pattern(regex),
   }),
 }), createCard);
+
 router.get('/cards', getCards);
+
 router.delete('/cards/:cardId', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).hex(),
   }),
 }), deleteCard);
+
 router.put('/cards/:cardId/likes', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).hex(),
   }),
 }), likeCard);
+
 router.delete('/cards/:cardId/likes', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).hex(),
