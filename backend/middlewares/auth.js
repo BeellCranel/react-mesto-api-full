5a2e636a7300d2996eb36c0d6189cf74125c9c05
Dergach Roman <dergachroma@gmail.com>
2022-05-21 17:43:48 +0300

    fix: переделал мидлвару авторизации под jwt из локального хранилища

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,8 +3,11 @@ const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
-  if (!token) next(new UnauthorizedError('Необходима авторизация'));
+  const { authorization } = req.headers;
+  const token = authorization.split(' ')[1];
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError('Необходима авторизация');
+  }
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
